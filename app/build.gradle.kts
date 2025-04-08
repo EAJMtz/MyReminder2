dc139e8a44300dce23f2864e571508e869c18b5c
EAJMtz <alberto_martinezjav@hotmail.com>
2025-04-07 22:59:21 -0600

    Cambio en archivos Kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,11 @@ android {
     namespace = "com.example.myreminder"
     compileSdk = 35
 
+    buildFeatures {
+        viewBinding = true
+    }
+
+
     defaultConfig {
         applicationId = "com.example.myreminder"
         minSdk = 24
